2a6c1f13bb2191cf198d347052a904e14927a2f3
aalok0003 <81555853+aalok0003@users.noreply.github.com>
2023-08-01 18:51:16 +0530

    Add files via upload

---
+++ college manager/college_manager.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from pages import attendence
+from pages import notes
+
+st.title("College Manager")
+
+st.header("About")
+st.write("Teachers may or may not mark your attendence seriously, but you can keep a track of it, so that you do not fall short on your attendance ever.")
+st.write("Note topics, important questions, deadlines etc")
