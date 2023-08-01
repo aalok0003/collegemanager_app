947f5ea11c21f3a25d9150720d48d0ab6e847877
aalok0003 <81555853+aalok0003@users.noreply.github.com>
2023-08-01 22:59:29 +0530

    Update notes.py

--- pages/notes.py
+++ pages/notes.py
@@ -9,6 +9,11 @@ client = pymongo.MongoClient('mongodb+srv://aalok:aalok@cluster0.cgn9day.mongodb
 db = client['class_notes']  # Replace 'attendance_db' with your preferred database name
 collection = db['class_notes']
 
+# Check if the user is logged in before allowing access to the page
+if not st.session_state.logged_in:
+    st.error("You must log in first to access this page.")
+    st.stop()
+
 st.title("Add Class Notes")
 
 # Get the current date from the device
@@ -23,8 +28,9 @@ selected_date = st.date_input("Select Date", value=current_date, key="date_selec
 notes = st.text_area("Add Notes")
 
 # Save the notes data to the database
-if st.button("Submit"):
+if st.button("Submit",key='button_key'):
     your_notes_data = {
+        "email": st.session_state.current_user,
         "subject": subject,
         "date": selected_date.strftime("%Y-%m-%d"),
         "attended": notes,
@@ -36,7 +42,7 @@ if st.button("Submit"):
 
 # Add a button to download all your attendance data as a CSV file
 if st.button("Fetch All Notes Data"):
-    all_notes_data = collection.find()
+    all_notes_data = collection.find({"email": st.session_state.current_user})
     all_data_list = list(all_notes_data)
     if all_data_list:
         df = pd.DataFrame(all_data_list)
