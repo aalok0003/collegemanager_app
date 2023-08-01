15c4225f7c5122432898cb7d0f4340203a23c041
aalok0003 <81555853+aalok0003@users.noreply.github.com>
2023-08-02 00:02:37 +0530

    Add files via upload

---
+++ pages/attendance.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import pymongo
+
+# Connect to MongoDB Atlas
+# Replace 'YOUR_CONNECTION_STRING' with your actual MongoDB Atlas connection string
+client = pymongo.MongoClient('mongodb+srv://aalok:aalok@cluster0.cgn9day.mongodb.net/?retryWrites=true&w=majority')
+db = client['attendance_db']  # Replace 'attendance_db' with your preferred database name
+collection = db['attendance_data']
+
+# Check if the user is logged in before allowing access to the page
+if not st.session_state.logged_in:
+    st.error("You must log in first to access this page.")
+    st.stop()
+
+st.title("Update Your Attendance")
+
+# Get the current date from the device
+current_date = datetime.now().date()
+
+subject = st.selectbox("Select Subject", ("Network Security and Cryptography", "Data Mining and Data Warehousing", "Elective-4", "Elective-5"))
+
+# Set the default value of the date input widget to the current date
+selected_date = st.date_input("Select Date", value=current_date)
+
+# Mark your attendance for the selected subject and date
+attended = st.checkbox("Attended", value=False)
+
+# Save the attendance data to the database
+if st.button("Submit"):
+    your_attendance_data = {
+        "email": st.session_state.current_user,
+        "subject": subject,
+        "date": selected_date.strftime("%Y-%m-%d"),
+        "attended": attended,
+    }
+    collection.insert_one(your_attendance_data)
+    st.success("Attendance data saved successfully!")
+
+
+
+# Add a button to download all your attendance data as a CSV file
+if st.button("Fetch All Attendance Data"):
+    your_attendance_data = collection.find({"email": st.session_state.current_user})
+    all_data_list = list(your_attendance_data)
+    if all_data_list:
+        df = pd.DataFrame(all_data_list)
+        st.dataframe(df)
+        df.to_csv("your_attendance_data.csv", index=False)
+        st.success("Attendance data downloaded successfully!")
+    else:
+        st.warning("No attendance data found to download.")
---
+++ pages/notes.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import pymongo
+
+# Connect to MongoDB Atlas
+# Replace 'YOUR_CONNECTION_STRING' with your actual MongoDB Atlas connection string
+client = pymongo.MongoClient('mongodb+srv://aalok:aalok@cluster0.cgn9day.mongodb.net/?retryWrites=true&w=majority')
+db = client['class_notes']  # Replace 'attendance_db' with your preferred database name
+collection = db['class_notes']
+
+# Check if the user is logged in before allowing access to the page
+if not st.session_state.logged_in:
+    st.error("You must log in first to access this page.")
+    st.stop()
+
+st.title("Add Class Notes")
+
+# Get the current date from the device
+current_date = datetime.now().date()
+
+subject = st.selectbox("Select Subject", ("Network Security and Cryptography", "Data Mining and Data Warehousing", "Elective-4", "Elective-5"),key="subject_selector")
+
+# Set the default value of the date input widget to the current date
+selected_date = st.date_input("Select Date", value=current_date, key="date_selector")
+
+# Add your notes for the selected subject and date
+notes = st.text_area("Add Notes")
+
+# Save the notes data to the database
+if st.button("Submit",key='button_key'):
+    your_notes_data = {
+        "email": st.session_state.current_user,
+        "subject": subject,
+        "date": selected_date.strftime("%Y-%m-%d"),
+        "attended": notes,
+    }
+    collection.insert_one(your_notes_data)
+    st.success("Notes data saved successfully!")
+
+
+
+# Add a button to download all your attendance data as a CSV file
+if st.button("Fetch All Notes Data"):
+    all_notes_data = collection.find({"email": st.session_state.current_user})
+    all_data_list = list(all_notes_data)
+    if all_data_list:
+        df = pd.DataFrame(all_data_list)
+        st.dataframe(df)
+        df.to_csv("your_notes_data.csv", index=False)
+        st.success("Notes data downloaded successfully!")
+    else:
+        st.warning("No notes data found to download.")
